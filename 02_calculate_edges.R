3a10613bbcfc435ab977854b8511d5d9ce454465
Alexa Fredston <alexa.fredstonhermann@gmail.com>
2024-06-20 10:45:55 -0700

    added bird stats and plots

--- 02_calculate_edges.R
+++ 02_calculate_edges.R
@@ -3,16 +3,19 @@ library(tidyverse)
 #library(ggnewscale)
 
 source(here("functions","calc_all_edges.R"))
+
+##########
+# FISH DATA
+##########
+
 load(here("data","fishdat.RData"))
 # hauldat <- read_csv(here("data","haul_data.csv"), col_types = cols(haul_id = col_character())) 
 
-fishdat <- dat 
+focal_fish <- c('Urophycis tenuis')
+fish_edgetype <- "eq"
 
-focal_spp <- c('Urophycis tenuis')
-edgetype <- "eq"
-
-dat <- as_tibble(fishdat) %>% 
-  filter(accepted_name==focal_spp) 
+fishdat <- as_tibble(dat) %>% 
+  filter(accepted_name==focal_fish) 
 
 # rm(dat_zeros)
 
@@ -21,16 +24,17 @@ dat <- as_tibble(fishdat) %>%
 # sapply(funs, function(x)
 #   source(file.path("functions", x)))
 
-dat <- dat %>% 
+fishdat <- fishdat %>% 
   filter(year>1971)
 
-edgetidy <- calc_all_edges(dat, edgetype, focal_spp)
+fishedgetidy <- calc_all_edges(fishdat, fish_edgetype, focal_fish)
 
-write_csv(edgetidy, file=here("results",paste0(focal_spp, "_", edgetype, "_results.csv")))
+write_csv(fishedgetidy, file=here("results",paste0(focal_fish, "_", fish_edgetype, "_results.csv")))
 
-# also note I'm not adding in flags for doing this with lots of species
-# for example, no flag to drop years without enough data points to calculate these metrics
-# since this is for illustration only 
+
+##########
+# BIRD DATA
+##########
 
 # choosing a bird
 
@@ -38,17 +42,29 @@ lasorte <- read_csv(here("data","la_sorte_thompson_2007_supp.csv")) #https://doi
 
 # TABLE A1. A table summarizing the associations and findings for all 254 avian species considered in the analysis. For each species, the migratory status (Migratory) and the center of each species winter range is indicated (N/S). Migratory status was determined based on examinations of winter and breeding distribution maps. The location of the center of the range was determined based on the average latitude of CBC circles in 1975 using 36º north latitude as the demarcation. In addition, the number of CBC circles is indicated where each species was designated as common (COM), common and extirpated (EXT), common and colonized (COL), and the number of CBC circles where the species was present but not identified in any of the three common categories (NCOM). These values were estimated based on an analysis of temporal turnover dynamics (see the text for details on the analysis and description of categories). Finally, slope coefficients from individual linear models are presented for each species that estimate latitudinal trends for the northern boundary (NB), center of occurrence (CO), and center of abundance (CA) of geographic ranges (see text for description of linear models and range components).
 
-spp_of_interest <- lasorte %>% 
-  filter(`N/S`=="S", # focus on southerly species so they don't run out of the study region
-         COM > 100, # and common species
-         NB > 0 # with a northward shift in 2007
-  )
+# spp_of_interest <- lasorte %>% 
+#   filter(`N/S`=="S", # focus on southerly species so they don't run out of the study region
+#          COM > 100, # and common species
+#          NB > 0 # with a northward shift in 2007
+#   )
 
 # looked up some range maps
 # let's focus on the black vulture--it definitively has a poleward edge in the US 
 
+focal_bird <- c('Coragyps atratus')
+bird_edgetype <- "pol"
+
 # download data from https://netapp.audubon.org/CBCObservation/Historical/ResultsBySpecies.aspx?1
-# as of personal communication with CBC team in june 2024, the data portal has a bug that doesn't allow data download later than the 115th CBC (2015), so I downloaded 1975 (the start year of La Sorte and Jetz) - 2014
+# the historical data download has some bugs so I used the "current year" tab and then went back and manually downloaded every count from 1975 to present for black vulture (as per pers comm w CBC)
 
+file_names <- dir(here("data","cbc_black_vulture_manual_download"), full.names=TRUE)
 
+birddat <- read_csv(file_names, id = "name", skip=3) %>% # first three rows have no data except saying "black vulture"
+  mutate(year = as.numeric(str_sub(sub(".csv","",name), start=-4)),
+         num_cpue = Number_By_Party_Hours, 
+         latitude = Latitude) %>% # extract year value from file name
+  select(latitude, year, num_cpue)
 
+birdedgetidy <- calc_all_edges(birddat, bird_edgetype, focal_bird)
+
+write_csv(birdedgetidy, file=here("results",paste0(focal_bird, "_", bird_edgetype, "_results.csv")))
--- 03_analyze_edges.R
+++ 03_analyze_edges.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 library(here)
 
-edgetype <- "eq"
-focal_spp <- c('Urophycis tenuis')
-edgetidy <- read_csv(file=here("results",paste0(focal_spp, "_", edgetype, "_results.csv"))) %>% 
+fish_edgetype <- "eq"
+focal_fish <- c('Urophycis tenuis')
+fish_edgetidy <- read_csv(file=here("results",paste0(focal_fish, "_", fish_edgetype, "_results.csv"))) %>% 
   mutate(sig = factor(sig, levels=c('yes','no')))
 
 # which were significant, and what were the coefficients?
-edgetidy %>% 
+fish_edgetidy %>% 
   select(Method, p.value, estimate, std.error) %>% 
   distinct()
 
 # report in km (approximate)
-edgetidy %>% 
+fish_edgetidy %>% 
   select(Method, p.value, estimate, std.error) %>% 
   distinct() %>% 
   mutate(estimate = estimate * 111,
@@ -22,11 +22,11 @@ edgetidy %>%
 
 colorblind_safe <- c( '#6699CC', '#004488', '#EECC66', '#994455', '#997700', '#EE99AA') # https://personal.sron.nl/~pault/
 
-title <- ifelse(edgetype=="eq", paste0(focal_spp, " equatorward edge"), paste0(focal_spp, " poleward edge"))
+title <- ifelse(fish_edgetype=="eq", paste0(focal_fish, " equatorward edge"), paste0(focal_fish, " poleward edge"))
 
 # once final species are chosen can italicize title more easily
 
-fishplot <- ggplot(edgetidy, aes(x=year, y=lat_position, color=Method, fill=Method, group=Method)) +
+fishplot <- ggplot(fish_edgetidy, aes(x=year, y=lat_position, color=Method, fill=Method, group=Method)) +
   geom_line(aes(linetype=sig), lwd=0.8)  +
   scale_color_manual(values=colorblind_safe) +
   scale_fill_manual(values=colorblind_safe) +
@@ -35,9 +35,67 @@ fishplot <- ggplot(edgetidy, aes(x=year, y=lat_position, color=Method, fill=Meth
   theme_bw() +
   theme(legend.position=c(0.38, 0.3),
         legend.background = element_rect(fill="transparent")) +
- # theme(legend.position="bottom",
- #   legend.position.inside = c(0.5, 0.5)) +
+  # theme(legend.position="bottom",
+  #   legend.position.inside = c(0.5, 0.5)) +
   NULL
 #fishplot
 ggsave(fishplot, filename=here("figures","fish_edge.png"), width=8, height=4, dpi=160)
 
+
+bird_edgetype <- "pol"
+focal_bird <- c('Coragyps atratus')
+bird_edgetidy <- read_csv(file=here("results",paste0(focal_bird, "_", bird_edgetype, "_results.csv"))) %>% 
+  mutate(sig = factor(sig, levels=c('yes','no')))
+
+# which were significant, and what were the coefficients?
+bird_edgetidy %>% 
+  select(Method, p.value, estimate, std.error) %>% 
+  distinct()
+
+# report in km (approximate)
+bird_edgetidy %>% 
+  select(Method, p.value, estimate, std.error) %>% 
+  distinct() %>% 
+  mutate(estimate = estimate * 111,
+         std.error = std.error * 111)
+
+# make plots
+
+colorblind_safe <- c( '#6699CC', '#004488', '#EECC66', '#994455', '#997700', '#EE99AA') # https://personal.sron.nl/~pault/
+
+title <- ifelse(bird_edgetype=="eq", paste0(focal_bird, " equatorward edge"), paste0(focal_bird, " poleward edge"))
+
+# once final species are chosen can italicize title more easily
+
+birdplot <- ggplot(bird_edgetidy, aes(x=year, y=lat_position, color=Method, fill=Method, group=Method)) +
+  geom_line(aes(linetype=sig), lwd=0.8)  +
+  scale_color_manual(values=colorblind_safe) +
+  scale_fill_manual(values=colorblind_safe) +
+  labs(title=title, x="Year", y="Latitude") +
+  guides(linetype="none",
+         fill=guide_legend(ncol=2),
+         color=guide_legend(ncol=2)) +
+  theme_bw() +
+  theme(legend.position=c(0.235, 0.84),
+        legend.background = element_rect(fill="transparent")) +
+  # theme(legend.position="bottom",
+  #   legend.position.inside = c(0.5, 0.5)) +
+  NULL
+
+ggsave(birdplot, filename=here("figures","bird_edge.png"), width=8, height=4, dpi=160)
+
+sd_dat <- bind_rows(bird_edgetidy, fish_edgetidy) %>% 
+  group_by(Species, Method) %>% 
+  summarise(SD = sd(lat_position))
+
+sd_gg_fish <- sd_dat %>%
+  filter(Species == focal_fish) %>% 
+  ggplot(aes(x=Method, y=SD, color=Method, fill=Method)) + 
+  geom_point() +
+  scale_color_manual(values=colorblind_safe) +
+  scale_fill_manual(values=colorblind_safe) +
+  labs(title=title, x="Method", y="Standard deviation of latitude") +
+  theme_bw() +
+  NULL
+  sd_gg_fish
+ 
